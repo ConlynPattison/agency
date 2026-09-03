e2d38a3145abc3babd96083507f1cb0390beb379
ConlynPattison <conlynrp@gmail.com>
2026-09-03 12:54:48 -0700

    Fix formatting on single agent

--- agent-single-node/src/agent_single_node/main.py
+++ agent-single-node/src/agent_single_node/main.py
@@ -2,23 +2,17 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 
-
 def get_llm(
-        model="gemma4",
-        base_url="http://localhost:11434",
-        reasoning=False
-    ) -> ChatOllama:
-    return ChatOllama(
-        model=model,
-        base_url=base_url,
-        verbose=True,
-        reasoning=reasoning
-    )
+    model="gemma4", base_url="http://localhost:11434", reasoning=False
+) -> ChatOllama:
+    return ChatOllama(model=model, base_url=base_url, verbose=True, reasoning=reasoning)
 
 
 def sys_prompt() -> SystemMessage:
-    return SystemMessage(content="""You are a helpful assistant that can reason and answer questions. 
-    If you don't know the answer, you should say "I don't know".""")
+    return SystemMessage(
+        content="""You are a helpful assistant that can reason and answer questions. 
+    If you don't know the answer, you should say "I don't know"."""
+    )
 
 
 def init_messages() -> list:
@@ -38,11 +32,14 @@ def main() -> None:
         for chunk in llm.stream(messages):
             if chunk.content:
                 print(chunk.content, end="", flush=True)
-                aggregate_msg = chunk if aggregate_msg is None else aggregate_msg + chunk
+                aggregate_msg = (
+                    chunk if aggregate_msg is None else aggregate_msg + chunk
+                )
 
         messages.append(AIMessage(content=aggregate_msg.content))
 
         print("\n\n---\n")
+
 
 if __name__ == "__main__":
     main()
